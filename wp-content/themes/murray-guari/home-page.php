3b3f0365b62a282a8e4a020390731398e72015f7
scottstreit <scott.streit@gmail.com>
2014-02-16 16:00:37 -0500

    Updates to grid to include 4 column no gutter layout

--- wp-content/themes/murray-guari/home-page.php
+++ wp-content/themes/murray-guari/home-page.php
@@ -62,12 +62,12 @@ Template Name: Custom Home Page
 								<a href="<?php bloginfo('siteurl'); ?>/news">View All News</a>
 							</h1>
 							<ul>
-								<li class="NewsBlog threecol first"><a href="/Blog/2014-02-14/51/Prevent_Medication_Errors">February 14, 2014</a><p>Prevent Medication Errors</p><div><a href="/Blog/2014-02-14/51/Prevent_Medication_Errors">BLOG</a></div></li>
-								<li class="threecol"><a href="/News/2014-02-16/167/West_Palm_Beach_Personal_Injury_Law_Firm_Sponsors_“Serve_An_Ace_for_St._Jude’s_Research_Hospital”_Event">February 16, 2014</a><p>West Palm Beach Personal Injury Law Firm Sponsors “Serve An Ace for St. Jude’s Research Hospital” Event</p><div><a href="/News/2014-02-16/167/West_Palm_Beach_Personal_Injury_Law_Firm_Sponsors_“Serve_An_Ace_for_St._Jude’s_Research_Hospital”_Event">PRESS</a></div>
+								<li class="NewsBlog threecol first no-gutter"><a href="/Blog/2014-02-14/51/Prevent_Medication_Errors">February 14, 2014</a><p>Prevent Medication Errors</p><div><a href="/Blog/2014-02-14/51/Prevent_Medication_Errors">BLOG</a></div></li>
+								<li class="threecol no-gutter"><a href="/News/2014-02-16/167/West_Palm_Beach_Personal_Injury_Law_Firm_Sponsors_“Serve_An_Ace_for_St._Jude’s_Research_Hospital”_Event">February 16, 2014</a><p>West Palm Beach Personal Injury Law Firm Sponsors “Serve An Ace for St. Jude’s Research Hospital” Event</p><div><a href="/News/2014-02-16/167/West_Palm_Beach_Personal_Injury_Law_Firm_Sponsors_“Serve_An_Ace_for_St._Jude’s_Research_Hospital”_Event">PRESS</a></div>
 								</li>
-								<li class="threecol"><a href="/News/2014-02-06/166/Trial_Attorney_Jason_Guari_Named_as_a_“Top_Lawyer”_in_2014_South_Florida_Legal_Guide">February 6, 2014</a><p>Trial Attorney Jason Guari Named as a “Top Lawyer” in 2014 South Florida Legal Guide</p><div><a href="/News/2014-02-06/166/Trial_Attorney_Jason_Guari_Named_as_a_“Top_Lawyer”_in_2014_South_Florida_Legal_Guide">PRESS</a></div>
+								<li class="threecol no-gutter"><a href="/News/2014-02-06/166/Trial_Attorney_Jason_Guari_Named_as_a_“Top_Lawyer”_in_2014_South_Florida_Legal_Guide">February 6, 2014</a><p>Trial Attorney Jason Guari Named as a “Top Lawyer” in 2014 South Florida Legal Guide</p><div><a href="/News/2014-02-06/166/Trial_Attorney_Jason_Guari_Named_as_a_“Top_Lawyer”_in_2014_South_Florida_Legal_Guide">PRESS</a></div>
 								</li>
-								<li class="NewsFacebook threecol"><a href="http://www.mypalmbeachpost.com/news/news/tracking-devices-pitch-driver-savings-but-bills-do/ndPpX/" target="_blank">February 16, 2014</a><p>The possibililty of #increasingrates and #privacy concerns complicate growing #insurance trend -use of #trackingdevices.</p><div><a href="http://www.mypalmbeachpost.com/news/news/tracking-devices-pitch-driver-savings-but-bills-do/ndPpX/" target="_blank">FACEBOOK</a></div>
+								<li class="NewsFacebook threecol no-gutter"><a href="http://www.mypalmbeachpost.com/news/news/tracking-devices-pitch-driver-savings-but-bills-do/ndPpX/" target="_blank">February 16, 2014</a><p>The possibililty of #increasingrates and #privacy concerns complicate growing #insurance trend -use of #trackingdevices.</p><div><a href="http://www.mypalmbeachpost.com/news/news/tracking-devices-pitch-driver-savings-but-bills-do/ndPpX/" target="_blank">FACEBOOK</a></div>
 								</li>
 							</ul>
 						</div>
