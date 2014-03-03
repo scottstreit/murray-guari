560d38ec3f0f806608f52c1c55aa15c950891840
Adrian Stride <a@mahi.cc>
2014-03-03 18:19:15 +0000

    Added Footer styles.

--- wp-content/themes/murray-guari/footer.php
+++ wp-content/themes/murray-guari/footer.php
@@ -1,12 +1,22 @@
 			<footer class="footer" role="contentinfo">
 
 				<div id="inner-footer" class="wrap clearfix">
-
-					<nav role="navigation">
-							<?php bones_footer_links(); ?>
-					</nav>
-
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<div class="eightcol first clearfix">
+						<h3>HOW CAN WE HELP</h3>
+						<h4>Serving accident victims with dedication and understanding.</h4>
+						<nav role="navigation">
+								<?php bones_footer_links(); ?>
+						</nav>
+					</div>
+					<div class="fourcol clearfix">
+						<h3>AFFILIATIONS</h3>
+						<div>
+							<img src="<?php bloginfo('siteurl'); ?>/wp-content/uploads/2014/03/MG-Affiliations1.jpg">
+						</div>
+					</div>
+					<div class="twelvecol">
+						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. Designed by <a href="http://www.bardmarketing.com">BARD Marketing</a></p>
+					</div>
 
 				</div>
 
