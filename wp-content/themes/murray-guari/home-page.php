11057798bfa269372935551df237fd654dd3039b
Adrian Stride <a@mahi.cc>
2014-03-03 16:09:53 +0000

    Added Blog Loop to home page.

--- wp-content/themes/murray-guari/home-page.php
+++ wp-content/themes/murray-guari/home-page.php
@@ -106,13 +106,16 @@ Template Name: Custom Home Page
 								<a href="<?php bloginfo('siteurl'); ?>/news">View All News</a>
 							</h1>
 							<ul>
-								<li class="NewsBlog threecol first no-gutter"><a href="/Blog/2014-02-14/51/Prevent_Medication_Errors">February 14, 2014</a><p>Prevent Medication Errors</p><div><a href="/Blog/2014-02-14/51/Prevent_Medication_Errors">BLOG</a></div></li>
-								<li class="threecol no-gutter"><a href="/News/2014-02-16/167/West_Palm_Beach_Personal_Injury_Law_Firm_Sponsors_“Serve_An_Ace_for_St._Jude’s_Research_Hospital”_Event">February 16, 2014</a><p>West Palm Beach Personal Injury Law Firm Sponsors “Serve An Ace for St. Jude’s Research Hospital” Event</p><div><a href="/News/2014-02-16/167/West_Palm_Beach_Personal_Injury_Law_Firm_Sponsors_“Serve_An_Ace_for_St._Jude’s_Research_Hospital”_Event">PRESS</a></div>
-								</li>
-								<li class="threecol no-gutter"><a href="/News/2014-02-06/166/Trial_Attorney_Jason_Guari_Named_as_a_“Top_Lawyer”_in_2014_South_Florida_Legal_Guide">February 6, 2014</a><p>Trial Attorney Jason Guari Named as a “Top Lawyer” in 2014 South Florida Legal Guide</p><div><a href="/News/2014-02-06/166/Trial_Attorney_Jason_Guari_Named_as_a_“Top_Lawyer”_in_2014_South_Florida_Legal_Guide">PRESS</a></div>
-								</li>
-								<li class="NewsFacebook threecol no-gutter"><a href="http://www.mypalmbeachpost.com/news/news/tracking-devices-pitch-driver-savings-but-bills-do/ndPpX/" target="_blank">February 16, 2014</a><p>The possibililty of #increasingrates and #privacy concerns complicate growing #insurance trend -use of #trackingdevices.</p><div><a href="http://www.mypalmbeachpost.com/news/news/tracking-devices-pitch-driver-savings-but-bills-do/ndPpX/" target="_blank">FACEBOOK</a></div>
-								</li>
+								<?php
+									$args = array('post_type' => 'post', 'posts_per_page' => 4);
+									$loop = new WP_Query($args);
+									while ($loop->have_posts()) : $loop->the_post();
+								?>
+									<li class="NewsBlog threecol no-gutter">
+										<a href="<?php the_permalink() ?>"><?php the_time('F jS, Y') ?></a>
+										<?php the_excerpt(); ?>
+									</li>
+								<?php endwhile; ?>
 							</ul>
 						</div>
 					</div>
