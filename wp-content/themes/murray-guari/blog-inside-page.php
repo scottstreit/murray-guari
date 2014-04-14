7bf41c0bd13c1ee2848a4afe6cec10880e2af96e
scottstreit <scott.streit@gmail.com>
2014-04-13 23:40:14 -0400

    Adding templates for blog and media coverage categories.

---
+++ wp-content/themes/murray-guari/blog-inside-page.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Template Name: Blog Page Template
+*/
+?>
+
+<?php get_header(); ?>
+
+      <div id="content">
+
+        <div id="inner-content" class="wrap clearfix">
+
+            <div id="main" class="eightcol first clearfix" role="main">
+              <h1>Blog Posts</h1>
+               <?php query_posts('cat=5'); ?>
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                <section class="entry-content clearfix" itemprop="articleBody">
+                  <?php the_content(); ?>
+                </section>
+
+              <?php endwhile; else : ?>
+
+                  <article id="post-not-found" class="hentry clearfix">
+                      <header class="article-header">
+                        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                    </header>
+                      <section class="entry-content">
+                        <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+                    </section>
+                    <footer class="article-footer">
+                        <p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
+                    </footer>
+                  </article>
+
+              <?php endif; ?>
+
+              <?php get_snippet('Bottom'); ?>
+            </div>
+            <br><br>
+            <?php get_sidebar(); ?>
+
+        </div>
+
+      </div>
+
+<?php get_footer(); ?>
