acf40ee60d680c08dd9cb10632b2aba3949951e4
scottstreit <scott.streit@gmail.com>
2014-06-02 21:39:42 -0400

    added google analytics to footer

--- wp-content/themes/murray-guari/footer.php
+++ wp-content/themes/murray-guari/footer.php
@@ -26,6 +26,17 @@
 
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
+		<script>
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-25215514-1']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+		</script>
 
 	</body>
 
